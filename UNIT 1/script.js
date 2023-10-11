acc4f29219c5eb2f20c18ddbd1023e9ca7bd5e94
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2023-10-11 20:56:06 +0300

    first commit

---
+++ UNIT 1/script.js
@@ -0,0 +1,61 @@
+console.log('Dmitry'); //1
+
+console.log(5 + " may");//2
+
+console.log('Добро ' + 'пожаловать ' + ' на курс');//3
+
+'alert(2019);//4'
+
+'alert(2019 - 200);//5'
+
+document.getElementById('one').innerHTML = "Hello world";//6
+
+document.querySelector('.one').innerHTML = "Hello world";//7
+
+document.querySelector('.two').innerHTML = 12 * 12;//8
+
+document.querySelector('span').innerHTML = 'world';//9
+
+document.querySelector('.three').innerHTML = '<h3>pen</h3>';//10
+
+document.querySelector('.four').innerHTML = '<h4>text</h4>' + '<p>paragraf</p>';//11
+
+let a = document.querySelector('.five');//12
+a.innerHTML = 3.1415;
+
+let div7 = document.querySelector('.seven');//13
+div7.innerHTML = '<img src="https://cdn4.iconfinder.com/data/icons/food-and-drink-flat-gradient/32/cone_ice_cream_food_drink_sweet-512.png" alt="">';
+
+let z1 = 6;//14
+let z2 = 3;
+document.querySelector('.eight').innerHTML = z1 + z2;
+
+let y1 = 6;//15
+let y2 = 3;
+document.querySelector('.nine').innerHTML = y1 + y2;
+
+let x1 = 'hello';//16
+let x2 = 5;
+document.querySelector('.ten').innerHTML = x1 + x2;
+
+let d1 = document.querySelector('.test-1');//17
+console.log(d1);
+
+let d2 = document.querySelector('.test-2');//18
+console.log(d2);
+d2 = 5;
+console.log(d2)
+
+let divS3 = document.querySelector('.s3');//19
+console.log(divS3)
+divS3 = document.querySelector('.s4');
+console.log(divS3);
+
+//document.querySelector('body').innerHTML = ' ';//20
+
+
+
+
+
+
+
