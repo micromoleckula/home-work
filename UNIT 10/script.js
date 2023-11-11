c018349a49970bd43073560fc1ad53d2d8e561b5
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2023-11-11 18:55:24 +0200

    learning

---
+++ UNIT 10/script.js
@@ -0,0 +1,34 @@
+// let a = 'Иван';
+// let b = '37';
+// let z7 = 'Овен';
+
+// let c = ['Ivan', 37, 'oven'];
+
+// let d = [];
+
+// console.log(c);
+// console.log(c.length)
+
+// let ua = {
+//   name: 'Dmitry',
+//   age: 18,
+//   work: 'pc-service',
+//   city: 'Dnipro',
+// }
+
+// c[1] = 'Peis'
+
+// console.log(ua)
+
+
+let a = [1, 2, 3, 4];
+console.log(a);
+
+
+let t = a[0];
+a[0] = a[a.length - 1];
+a[a.length - 1] = t
+
+
+
+console.log(a);
