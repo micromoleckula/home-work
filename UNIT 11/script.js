033b8d6b34f4bc230ccca2e881f767918b50f76c
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2023-11-19 23:51:06 +0200

    UNIT 11 task 8 9 10 done

--- UNIT 11/script.js
+++ UNIT 11/script.js
@@ -74,18 +74,45 @@ document.querySelector('.b-6').onclick = f6;
 7
 d7 = ['china', 'india', 'brazil', 'japan', 'egypt'];
 
-function f6() {
-  let i6 = document.querySelector('.i-6').value;
-  
-  if (i6 !== "") {
-    d6[d6.length] = i6;
-    showArr('.out-6', d6);
+function f7() {
+  d7 = d7.slice(0, -1);
+  showArr('.out-7', d7);
+}
+
+document.querySelector('.b-7').onclick = f7;
+
+8
+let d8 = [2, '4', 12, 67, 'hello'];
+
+function f8() {
+  let i8 = document.querySelector('.i-8').value;
+
+  if (i8 !== "") {
+    d8 = [i8, ...d8];
+    showArr('.out-8', d8);
   } else {
     alert("Введите значение");
   }
 }
 
+document.querySelector('.b-8').onclick = f8;
 
+9
+d9 = [100, 200, 300, 400, 700, 121];
 
+function f9() {
+  d9 = d9.slice(0, -1);
+  showArr('.out-9', d9);
+}
 
+document.querySelector('.b-9').onclick = f9;
 
+10
+d10 = [3,14,15,92,6];
+
+function f10() {
+  d10.reverse();
+  showArr('.out-10', d10);
+}
+
+document.querySelector('.b-10').onclick = f10;
