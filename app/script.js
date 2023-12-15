97cf2081488bb860ebd22df7da1c5ab4e0982781
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2023-12-15 22:58:27 +0200

    preEnd unit 14

---
+++ app/script.js
@@ -0,0 +1,13 @@
+fetch('http://api.openweathermap.org/data/2.5/weather?id=703448&appid=70e1ed322b02acbc57d443dd91065f3e')
+    .then(function (resp) { return resp.json() })
+    .then(function (data) {
+        console.log(data);
+        document.querySelector('.package-name').textContent = data.name;
+        document.querySelector('.price').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
+        document.querySelector('.disclaimer').textContent = data.weather[0]['description'];
+        //https://openweathermap.org/img/wn/02d@2x.png
+        document.querySelector('.features li').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+    })
+    .catch(function () {
+        // catch any errors
+    });
