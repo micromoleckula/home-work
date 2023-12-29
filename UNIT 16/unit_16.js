b977cc24f2ea4d12fa0ff7104838f4f81de2cc5f
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2023-12-29 23:29:45 +0200

    task 5

--- UNIT 16/unit_16.js
+++ UNIT 16/unit_16.js
@@ -49,7 +49,7 @@ function f4() {
 }
 
 document.querySelector('.b-4').addEventListener('click', f4);
-;
+
 
 // Task 5
 //При нажатии .b-5 выполняете функцию f5. Функция должна с помощью for of перебрать массив a5 и возвратить новый массив куда входят элементы из a5 большие 7.
@@ -57,7 +57,13 @@ document.querySelector('.b-4').addEventListener('click', f4);
 let a5 = [3,4,5,2,1,7,8,2,4,6,8,11,23,17];
 
 function f5() {
-
+    let out = [];
+    for (const i of a5) {
+        if (i > 7) {
+            out.push(i); 
+        }
+    }
+    return out;
 }
 
 document.querySelector('.b-5').addEventListener('click', ()=> {
