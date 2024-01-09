074e43186651b5b6694beab6d36ff9bb81ea0ab7
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2024-01-09 23:56:12 +0200

    work on task

--- UNIT 17/unit_17.js
+++ UNIT 17/unit_17.js
@@ -51,7 +51,9 @@ document.querySelector('.b-2').onclick = () => {
 let a3 = [4,"3",6,7,"12",34,"56",78,90,11];
 
 function t3() {
-
+    a3_res = a3.map(elem => {
+        return parseInt(a3)
+    })
 }
 
 document.querySelector('.b-3').onclick = () => {
