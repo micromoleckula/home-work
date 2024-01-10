31b6930f0c6d8a73d7adf2a23b36f8ec180c67f3
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2024-01-10 23:01:03 +0200

    task 3

--- UNIT 17/unit_17.js
+++ UNIT 17/unit_17.js
@@ -52,8 +52,9 @@ let a3 = [4,"3",6,7,"12",34,"56",78,90,11];
 
 function t3() {
     a3_res = a3.map(elem => {
-        return parseInt(a3)
+        return parseInt(elem)
     })
+    return a3_res
 }
 
 document.querySelector('.b-3').onclick = () => {
